26380b752bbaf34e38d941c7d6d7fe914bedfd5b
Thinh Vu <Thinhvu1198@gmail.com>
2023-10-16 00:19:48 +0100

    added dynamic html

--- portfolio/MyWeb/views.py
+++ portfolio/MyWeb/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django import forms
+
+class ContactForms(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea())
+
+    def __init__(self, *args, **kwargs):
+        super(ContactForms, self).__init__(*args, **kwargs)
+        for visiable in self.visible_fields():
+            visiable.field.widget.attrs['class'] = 'form-input'
+            visiable.field.widget.attrs['autocomplete'] = 'off'
+            visiable.field.widget.attrs['placeholder'] = visiable.label
+            visiable.label = False
 
 # Create your views here.
-
 def index(request):
-    return render(request, 'page/index.html')
+    return render(request, 'page/index.html', {
+        'form': ContactForms()
+    })
