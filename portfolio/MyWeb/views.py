65969c012018599829ff4d06d2326a9c7a270f57
Thinh Vu <Thinhvu1198@gmail.com>
2023-10-17 02:15:13 +0100

    playing around w colours

--- portfolio/MyWeb/views.py
+++ portfolio/MyWeb/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render
 from django import forms
 
 class ContactForms(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField()    
     email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea())
+    message = forms.CharField()
 
     def __init__(self, *args, **kwargs):
         super(ContactForms, self).__init__(*args, **kwargs)
-        for visiable in self.visible_fields():
-            visiable.field.widget.attrs['class'] = 'form-input'
-            visiable.field.widget.attrs['autocomplete'] = 'off'
-            visiable.field.widget.attrs['placeholder'] = visiable.label
-            visiable.label = False
+
+        self.fields['message'].widget = forms.Textarea()
+
+        for visible in self.visible_fields():
+            visible.field.widget.attrs = {
+                'class': 'form-input', 
+                'placeholder': visible.label,
+                'rows': 5
+            }   
+            visible.label = False
 
 # Create your views here.
 def index(request):
